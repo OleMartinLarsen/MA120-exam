8a168ef45ef82fa5306f64cda38c362f95a292b1
Ole Martin Larsen <olemartinlarsen@outlook.com>
2019-10-03 15:55:57 +0200

    added comments

--- 1a_WordCount/mapper.py
+++ 1a_WordCount/mapper.py
@@ -2,24 +2,36 @@ import sys
 from lxml import etree
 import re
 
+
 sys.stdin = sys.stdin.detach()
+
+# parse the data from xml document
 tree = etree.parse(sys.stdin)
+
+# get root node of document
 root = tree.getroot()
 
 
 def strip_non_ascii(string):
-    ''' Returns the string without non ASCII characters'''
+    # Returns the string without non ASCII characters
     stripped = (c for c in string if 0 < ord(c) < 127)
     return ''.join(stripped)
 
 
 for child in root:
+    # sorts rows on posttypeid
     if child.get("PostTypeId") == "1":
+        # extracts the body attribute
         body = child.get("Body")
+        # removes tags
         body = re.sub("<.*?>", '', body)
+        # removes punctation
         body = re.sub(r'[^\w\s]', '', body)
         body = strip_non_ascii(body)
+        # remove leading and trailing whitespace
+        # make a list of strings
         words = body.strip().split()
 
+        # print the words you have left after cleaning words
         for word in words:
             print('%s\t%s' % (word.lower(), "1"))
--- 1a_WordCount/reducer.py
+++ 1a_WordCount/reducer.py
@@ -6,20 +6,31 @@ wordcount = {}
 
 for line in sys.stdin:
 
-    word, count = line.split('\t', 1)
-
+    # parse the input we got from mapper.py
+    # remove leading and trailing whitespace
+    # make a list of the strings
+    word, count = line.strip().split('\t', 1)
     try:
+        # convert count (currently a string) to int
         count = int(count)
     except ValueError:
+        # count was not a number
+        # ignore/discard this line
         continue
 
     try:
+        # if multiple words add then count together
+        # this is made possible becuase sorts the map output by key before
+        # passed to the reducer
         wordcount[word] = wordcount[word]+count
     except:
         wordcount[word] = count
 
+
+# sorting the wordcount by value
 wordcount = OrderedDict(
     sorted(wordcount.items(), key=itemgetter(1), reverse=True))
 
+# printing the wordcount
 for word in wordcount.keys():
     print('%s\t%s' % (word, wordcount[word]))
