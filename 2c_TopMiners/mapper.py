7f1cf6622035748308037598a6e677853be1fb67
Ole Martin Larsen <olemartinlarsen@outlook.com>
2019-10-05 17:38:51 +0200

    top 10 miners done

---
+++ 2c_TopMiners/mapper.py
@@ -0,0 +1,14 @@
+import sys
+from lxml import etree
+import re
+
+'''parse the data from xml document'''
+tree = etree.parse(sys.stdin)
+root = tree.getroot()
+
+for child in root:
+    '''extracts id attribute from user'''
+    username = child.get("DisplayName")
+    reputation = child.get("Reputation")
+
+    print('{}\t{}'.format(username, reputation))
---
+++ 2c_TopMiners/reducer.py
@@ -0,0 +1,33 @@
+import sys
+from collections import OrderedDict
+from collections import Counter
+from operator import itemgetter
+
+reputations = {}
+
+
+def sort_miners(data):
+    '''sorting miners and get top 10 based on reputation'''
+    top10 = OrderedDict(
+        sorted(data.items(), key=itemgetter(1), reverse=True)[:10])
+    return top10
+
+
+for line in sys.stdin:
+
+    '''parse the input we got from mapper.py'''
+    username, reputation = line.strip().split('\t', 1)
+
+    try:
+        reputation = int(reputation)
+    except ValueError:
+        '''reputation was not a number'''
+        continue
+
+    reputations[username] = reputation
+
+
+top10 = sort_miners(reputations)
+
+for user in top10.keys():
+    print('{}\t{}'.format(user, reputations[user]))
