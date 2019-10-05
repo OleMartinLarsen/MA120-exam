6f9c3cc926dbf8eee45a6200b3e3b3115ce6d815
Ole Martin Larsen <olemartinlarsen@outlook.com>
2019-10-05 18:09:58 +0200

    favorite questions donee

---
+++ 2e_FavoriteQuestions/mapper.py
@@ -0,0 +1,15 @@
+import sys
+from lxml import etree
+import re
+
+'''parse the data from xml document'''
+tree = etree.parse(sys.stdin)
+root = tree.getroot()
+
+for child in root:
+    '''extracts title and favourite count attribute from post'''
+    if child.get("PostTypeId") == '1':
+        question = child.get('Title')
+        favourite_count = child.get('FavoriteCount')
+
+        print('{}\t{}'.format(question, favourite_count))
---
+++ 2e_FavoriteQuestions/reducer.py
@@ -0,0 +1,33 @@
+import sys
+from collections import OrderedDict
+from collections import Counter
+from operator import itemgetter
+
+favorite_counts = {}
+
+
+def top10(data):
+    '''sorting questions based on favorite count and get top 10 questions'''
+    top10 = OrderedDict(
+        sorted(data.items(), key=itemgetter(1), reverse=True)[:10])
+    return top10
+
+
+for line in sys.stdin:
+
+    '''parse the input we got from mapper.py'''
+    question, favorite_count = line.strip().split('\t', 1)
+
+    try:
+        favorite_count = int(favorite_count)
+    except ValueError:
+        '''favorite_count was not a number'''
+        continue
+
+    favorite_counts[question] = favorite_count
+
+
+top10 = top10(favorite_counts)
+
+for question in top10.keys():
+    print('{}\t{}'.format(question, favorite_counts[question]))
