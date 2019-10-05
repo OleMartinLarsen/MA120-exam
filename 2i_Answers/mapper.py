dfbdc7681edc4b9146d1f00fd4e5e7346aea39d7
Ole Martin Larsen <olemartinlarsen@outlook.com>
2019-10-05 21:14:07 +0200

    answers done

---
+++ 2i_Answers/mapper.py
@@ -0,0 +1,14 @@
+import sys
+from lxml import etree
+import re
+
+'''parse the data from xml document'''
+tree = etree.parse(sys.stdin)
+root = tree.getroot()
+
+for child in root:
+    '''extracts title attribute from post to
+    questions with more than 0 answers'''
+    if child.get('PostTypeId') == '1' and child.get('AnswerCount') != '0':
+
+        print('{}'.format('1'))
---
+++ 2i_Answers/reducer.py
@@ -0,0 +1,22 @@
+import sys
+from collections import OrderedDict
+from collections import Counter
+from operator import itemgetter
+
+counter = 0
+
+for line in sys.stdin:
+
+    '''parse the input we got from mapper.py'''
+    count = line
+
+    try:
+        count = int(count)
+    except ValueError:
+        '''count was not a number'''
+        continue
+
+    counter = counter + count
+
+
+print('{}{}'.format(counter, ' questions have at least one answer.'))
